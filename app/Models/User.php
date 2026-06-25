1dbecae0c41f04aaec03fe8a5ed4a415b575860f
Hiikey7 <kelvinramsiel@gmail.com>
2026-06-25 21:44:47 +0300

    Overhaul User model with Spatie traits, staff relationships, work_email support, and mail routing fallback

--- app/Models/User.php
+++ app/Models/User.php
@@ -7,20 +7,92 @@ use Illuminate\Contracts\Auth\MustVerifyEmail;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
+use Illuminate\Notifications\Notification;
 use Laravel\Fortify\TwoFactorAuthenticatable;
+use Laravel\Sanctum\HasApiTokens;
+use Spatie\Permission\Traits\HasRoles;
 
 class User extends Authenticatable implements MustVerifyEmail
 {
     /** @use HasFactory<UserFactory> */
-    use HasFactory, Notifiable, TwoFactorAuthenticatable;
+    use HasApiTokens, HasFactory, HasRoles, Notifiable, TwoFactorAuthenticatable;
+
+    public function authoredIdeas()
+    {
+        return $this->hasMany(Idea::class, 'author_id');
+    }
+
+    public function changeRequests()
+    {
+        return $this->hasMany(ChangeRequest::class);
+    }
+
+    public function collaborationRequests()
+    {
+        return $this->hasMany(CollaborationRequest::class);
+    }
+
+    public function otpCodes()
+    {
+        return $this->hasMany(OtpCode::class);
+    }
+
+    public function staff()
+    {
+        return $this->hasOne(Staff::class);
+    }
+
+    public function createdRegions()
+    {
+        return $this->hasMany(Region::class, 'created_by');
+    }
+
+    public function createdDirectorates()
+    {
+        return $this->hasMany(Directorate::class, 'created_by');
+    }
+
+    public function createdDepartments()
+    {
+        return $this->hasMany(Department::class, 'created_by');
+    }
+
+    public function createdContractTypes()
+    {
+        return $this->hasMany(ContractType::class, 'created_by');
+    }
+
+    protected $fillable = [
+        'name',
+        'email',
+        'work_email',
+        'mobile_number',
+        'gender',
+        'password',
+        'onboarding_completed_at',
+    ];
+
+    protected $hidden = [
+        'password',
+        'remember_token',
+        'two_factor_recovery_codes',
+        'two_factor_secret',
+    ];
 
     protected function casts(): array
     {
         return [
             'email_verified_at' => 'datetime',
+            'work_email_verified_at' => 'datetime',
+            'onboarding_completed_at' => 'datetime',
             'password' => 'hashed',
             'two_factor_confirmed_at' => 'datetime',
         ];
+    }
+
+    public function routeNotificationForMail(Notification $notification): array|string
+    {
+        return $this->email ?? $this->work_email;
     }
 
     public function receivesBroadcastNotificationsOn(): array
