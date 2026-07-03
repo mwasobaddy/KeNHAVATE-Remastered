51ab98d580ccc62f73d8185264a1f5dad70df553
Hiikey7 <kelvinramsiel@gmail.com>
2026-07-03 06:33:35 +0300

    feat: add review api controller

---
+++ app/Http/Controllers/Api/Ideas/ReviewController.php
@@ -0,0 +1,36 @@
+<?php
+
+namespace App\Http\Controllers\Api\Ideas;
+
+use App\Http\Controllers\Controller;
+use App\Services\Ideas\IdeaService;
+use Illuminate\Http\JsonResponse;
+use Illuminate\Http\Request;
+
+class ReviewController extends Controller
+{
+    public function __construct(
+        private IdeaService $ideaService,
+    ) {}
+
+    public function index(Request $request): JsonResponse
+    {
+        $user = $request->user();
+
+        $data = [];
+
+        if ($user->can('idea.assign_officer')) {
+            $data['pending_assignment'] = $this->ideaService->getPendingAssignment();
+        }
+
+        if ($user->can('idea.classify')) {
+            $data['my_assignments'] = $this->ideaService->getMyAssignments($user);
+        }
+
+        if ($user->can('idea.dg_decision')) {
+            $data['pending_decisions'] = $this->ideaService->getPendingDecisions();
+        }
+
+        return response()->json($data);
+    }
+}
