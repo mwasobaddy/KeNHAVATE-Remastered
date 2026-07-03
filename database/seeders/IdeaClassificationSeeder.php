bfc0724f45af9f1b9847c20caade6c049b48c911
Hiikey7 <kelvinramsiel@gmail.com>
2026-07-03 06:33:36 +0300

    feat: seed idea classification lookup

---
+++ database/seeders/IdeaClassificationSeeder.php
@@ -0,0 +1,42 @@
+<?php
+
+namespace Database\Seeders;
+
+use App\Models\IdeaClassification;
+use Illuminate\Database\Seeder;
+
+class IdeaClassificationSeeder extends Seeder
+{
+    public function run(): void
+    {
+        $classifications = [
+            [
+                'name' => 'Innovation',
+                'slug' => 'innovation',
+                'description' => 'Small-scale, testable idea within KeNHA control',
+            ],
+            [
+                'name' => 'Research',
+                'slug' => 'research',
+                'description' => 'Idea requiring testing, evidence, or standards input',
+            ],
+            [
+                'name' => 'Project',
+                'slug' => 'project',
+                'description' => 'Capital intensive, system-wide, requiring Management or Board direction',
+            ],
+            [
+                'name' => 'Outside Mandate',
+                'slug' => 'outside_mandate',
+                'description' => 'Idea not falling within KeNHA\'s statutory functions',
+            ],
+        ];
+
+        foreach ($classifications as $classification) {
+            IdeaClassification::firstOrCreate(
+                ['slug' => $classification['slug']],
+                $classification,
+            );
+        }
+    }
+}
