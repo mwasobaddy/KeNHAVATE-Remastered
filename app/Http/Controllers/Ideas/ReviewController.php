b7eebb8e69501dd67cbb65e93485e70ed692cb7f
Hiikey7 <kelvinramsiel@gmail.com>
2026-07-03 06:33:35 +0300

    feat: add review web controller

---
+++ app/Http/Controllers/Ideas/ReviewController.php
@@ -0,0 +1,43 @@
+<?php
+
+namespace App\Http\Controllers\Ideas;
+
+use App\Http\Controllers\Controller;
+use App\Services\Ideas\IdeaService;
+use Illuminate\Http\Request;
+use Inertia\Response;
+
+class ReviewController extends Controller
+{
+    public function __construct(
+        private IdeaService $ideaService,
+    ) {}
+
+    public function index(Request $request): Response
+    {
+        $user = $request->user();
+        $tab = $request->query('tab', 'pending-assignment');
+
+        $pendingAssignment = $user->can('idea.assign_officer')
+            ? $this->ideaService->getPendingAssignment()
+            : null;
+
+        $myAssignments = $user->can('idea.classify')
+            ? $this->ideaService->getMyAssignments($user)
+            : null;
+
+        $pendingDecisions = $user->can('idea.dg_decision')
+            ? $this->ideaService->getPendingDecisions()
+            : null;
+
+        return inertia('ideas/review', [
+            'currentTab' => $tab,
+            'pendingAssignment' => $pendingAssignment,
+            'myAssignments' => $myAssignments,
+            'pendingDecisions' => $pendingDecisions,
+            'canAssign' => $user->can('idea.assign_officer'),
+            'canClassify' => $user->can('idea.classify'),
+            'canDecide' => $user->can('idea.dg_decision'),
+        ]);
+    }
+}
