da41e52956b9f98754a4f0df4d9d209735e2e9d3
Hiikey7 <kelvinramsiel@gmail.com>
2026-07-03 06:33:35 +0300

    feat: seed idea classifications

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -17,6 +17,7 @@ class DatabaseSeeder extends Seeder
             ContractTypeSeeder::class,
             PointSeeder::class,
             IdeaCategorySeeder::class,
+            IdeaClassificationSeeder::class,
         ]);
     }
 }
