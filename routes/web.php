2717c4e794722021fbd9f729907a68a8904e3aed
Hiikey7 <kelvinramsiel@gmail.com>
2026-07-06 12:50:14 +0300

    feat: add role, user, and collaboration routes

--- routes/web.php
+++ routes/web.php
@@ -11,12 +11,15 @@ use App\Http\Controllers\Ideas\AssignmentController;
 use App\Http\Controllers\Ideas\ChangeRequestController;
 use App\Http\Controllers\Ideas\ClassificationController;
 use App\Http\Controllers\Ideas\CollaborationController;
+use App\Http\Controllers\Ideas\CollaborationRequestController;
 use App\Http\Controllers\Ideas\IdeaController;
 use App\Http\Controllers\Ideas\InvitationController;
 use App\Http\Controllers\Ideas\ReviewController;
 use App\Http\Controllers\Points\LeaderboardController;
 use App\Http\Controllers\Points\PointController;
 use App\Http\Controllers\Points\TransactionController;
+use App\Http\Controllers\Roles\RoleController;
+use App\Http\Controllers\Users\UserController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
@@ -107,11 +110,67 @@ Route::middleware(['auth', 'verified', 'onboarding.complete', 'terms'])->group(f
         ->name('audit.index')
         ->middleware('permission:audit.view');
 
+    Route::prefix('roles')->name('roles.')->group(function () {
+        Route::get('/', [RoleController::class, 'index'])
+            ->name('index')
+            ->middleware('permission:role.manage|role.create|role.edit|role.delete');
+
+        Route::get('/create', [RoleController::class, 'create'])
+            ->name('create')
+            ->middleware('permission:role.create');
+
+        Route::post('/', [RoleController::class, 'store'])
+            ->name('store')
+            ->middleware('permission:role.create');
+
+        Route::get('/{role}/edit', [RoleController::class, 'edit'])
+            ->name('edit')
+            ->middleware('permission:role.edit');
+
+        Route::put('/{role}', [RoleController::class, 'update'])
+            ->name('update')
+            ->middleware('permission:role.edit');
+
+        Route::delete('/{role}', [RoleController::class, 'destroy'])
+            ->name('destroy')
+            ->middleware('permission:role.delete');
+    });
+
+    Route::prefix('users')->name('users.')->group(function () {
+        Route::get('/', [UserController::class, 'index'])
+            ->name('index')
+            ->middleware('permission:user.manage|user.create|user.edit|user.delete');
+
+        Route::get('/create', [UserController::class, 'create'])
+            ->name('create')
+            ->middleware('permission:user.create');
+
+        Route::post('/', [UserController::class, 'store'])
+            ->name('store')
+            ->middleware('permission:user.create');
+
+        Route::get('/{user}/edit', [UserController::class, 'edit'])
+            ->name('edit')
+            ->middleware('permission:user.edit');
+
+        Route::put('/{user}', [UserController::class, 'update'])
+            ->name('update')
+            ->middleware('permission:user.edit');
+
+        Route::delete('/{user}', [UserController::class, 'destroy'])
+            ->name('destroy')
+            ->middleware('permission:user.delete');
+    });
+
     Route::prefix('ideas')->name('ideas.')->group(function () {
         Route::get('/', [IdeaController::class, 'index'])->name('index');
         Route::get('/review', [ReviewController::class, 'index'])->name('review');
         Route::get('/create', [IdeaController::class, 'create'])->name('create');
         Route::post('/', [IdeaController::class, 'store'])->name('store');
+
+        Route::get('/collaborations/inbox', [CollaborationRequestController::class, 'inbox'])->name('collaborations.inbox');
+        Route::get('/collaborations/outbox', [CollaborationRequestController::class, 'outbox'])->name('collaborations.outbox');
+
         Route::get('/{slug}', [IdeaController::class, 'show'])->name('show');
         Route::get('/{slug}/edit', [IdeaController::class, 'edit'])->name('edit');
         Route::put('/{slug}', [IdeaController::class, 'update'])->name('update');
