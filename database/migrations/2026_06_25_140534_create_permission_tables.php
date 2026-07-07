b352ebeba8b9812cc8b2a2535fce3ef4d96c7c60
Hiikey7 <kelvinramsiel@gmail.com>
2026-07-07 20:11:28 +0300

    feat: add description column to permissions table
    - Add nullable text description column to permissions table

--- database/migrations/2026_06_25_140534_create_permission_tables.php
+++ database/migrations/2026_06_25_140534_create_permission_tables.php
@@ -27,6 +27,7 @@ return new class extends Migration
             $table->id(); // permission id
             $table->string('name');
             $table->string('guard_name');
+            $table->text('description')->nullable();
             $table->timestamps();
 
             $table->unique(['name', 'guard_name']);
