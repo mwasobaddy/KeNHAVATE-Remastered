092349f0e6301cdbd754ed25c048fb89fccec4bf
Hiikey7 <kelvinramsiel@gmail.com>
2026-07-17 11:22:32 +0300

    chore: register IdeaDemoSeeder in DatabaseSeeder

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -18,6 +18,7 @@ class DatabaseSeeder extends Seeder
             PointSeeder::class,
             IdeaCategorySeeder::class,
             IdeaClassificationSeeder::class,
+            IdeaDemoSeeder::class,
         ]);
     }
 }
