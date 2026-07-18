930fb3eed7856d7233fe2ef8e795ac0f09c141bc
Hiikey7 <kelvinramsiel@gmail.com>
2026-07-18 14:09:34 +0300

    refactor: update StoreDecisionRequest validation

--- app/Http/Requests/Ideas/Decisions/StoreDecisionRequest.php
+++ app/Http/Requests/Ideas/Decisions/StoreDecisionRequest.php
@@ -2,13 +2,24 @@
 
 namespace App\Http\Requests\Ideas\Decisions;
 
+use App\Models\Idea;
 use Illuminate\Foundation\Http\FormRequest;
 
 class StoreDecisionRequest extends FormRequest
 {
     public function authorize(): bool
     {
-        return $this->user()?->can('idea.record_decision') ?? false;
+        $slug = $this->route('slug');
+
+        if (! $slug) {
+            return false;
+        }
+
+        $idea = Idea::where('slug', $slug)->first();
+
+        return $idea
+            && $this->user()?->can('idea.record_decision')
+            && $idea->assigned_officer_id === $this->user()?->id;
     }
 
     public function rules(): array
