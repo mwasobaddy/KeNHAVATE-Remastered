4b62a04a506087bb23dbb91fb2b390bfaca4d13a
Hiikey7 <kelvinramsiel@gmail.com>
2026-07-18 14:09:32 +0300

    refactor: update ClassifyIdeaRequest validation rules

--- app/Http/Requests/Ideas/ClassifyIdeaRequest.php
+++ app/Http/Requests/Ideas/ClassifyIdeaRequest.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\Requests\Ideas;
 
+use App\Models\Idea;
 use Illuminate\Foundation\Http\FormRequest;
 use Illuminate\Validation\Rule;
 
@@ -9,7 +10,17 @@ class ClassifyIdeaRequest extends FormRequest
 {
     public function authorize(): bool
     {
-        return $this->user()?->can('idea.classify') ?? false;
+        $slug = $this->route('slug');
+
+        if (! $slug) {
+            return false;
+        }
+
+        $idea = Idea::where('slug', $slug)->first();
+
+        return $idea
+            && $this->user()?->can('idea.classify')
+            && $idea->assigned_officer_id === $this->user()?->id;
     }
 
     public function rules(): array
